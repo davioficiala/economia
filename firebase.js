8ad074cdae50a028678de4c4aca3f359f2e5bd65
davioficiala <daviwalker813@gmail.com>
2026-07-11 15:47:41 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,25 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs
+} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyAE2VcJyqu01rtqlgVoMg634FFfTGxiRgc",
+  authDomain: "brain-orcamento.firebaseapp.com",
+  projectId: "brain-orcamento",
+  storageBucket: "brain-orcamento.firebasestorage.app",
+  messagingSenderId: "991853359315",
+  appId: "1:991853359315:web:b2270aab447a853b212426"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+window.db = db;
+window.collection = collection;
+window.addDoc = addDoc;
+window.getDocs = getDocs;
